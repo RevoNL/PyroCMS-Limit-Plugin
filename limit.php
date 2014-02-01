e6b99c53b726a5d1f9aef2d0079a5f9a04b48dff
RevoNL <RevoNL@users.noreply.github.com>
2014-02-01 13:09:50 +0100

    Initial commit.

---
+++ limit.php
@@ -0,0 +1,100 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Limit Plugin
+ *
+ * Limit words
+ *
+ * @author		RevoNL
+ * @package		PyroCMS\Addon\Plugins
+ * @copyright	Copyright (c) 2014, RevoNL
+ */
+class Plugin_Limit extends Plugin
+{
+	public $version = '1.0.0';
+
+	public $name = array(
+		'en'	=> 'Limit'
+	);
+
+	public $description = array(
+		'en'	=> 'Limit words.'
+	);
+
+	/**
+	 * Returns a PluginDoc array that PyroCMS uses 
+	 * to build the reference in the admin panel
+	 *
+	 * All options are listed here but refer 
+	 * to the Blog plugin for a larger example
+	 *
+	 * @return array
+	 */
+	public function _self_doc()
+	{
+		$info = array(
+			'words' => array(
+				'description' => array(// a single sentence to explain the purpose of this method
+					'en' => 'Limit words and add custom ending. Example: {{ limit:words words="25" ending="<a href=\'/read-more\'>Read more</a>" }}'
+				),
+				'single' => false,// will it work as a single tag?
+				'double' => true,// how about as a double tag?
+				'variables' => 'This is the text you want to limit.',// list all variables available inside the double tag. Separate them|like|this
+				'attributes' => array(
+					'words' => array(// this is the name="World" attribute
+						'type' => 'number',// Can be: slug, number, flag, text, array, any.
+						'flags' => '',// flags are predefined values like asc|desc|random.
+						'default' => '',// this attribute defaults to this if no value is given
+						'required' => false,// is this attribute required?
+					),
+					'ending' => array(// this is the name="World" attribute
+						'type' => 'text',// Can be: slug, number, flag, text, array, any.
+						'flags' => '',// flags are predefined values like asc|desc|random.
+						'default' => '...',// this attribute defaults to this if no value is given
+						'required' => false,// is this attribute required?
+					),
+					'strip' => array(// this is the name="World" attribute
+						'type' => 'text',// Can be: slug, number, flag, text, array, any.
+						'flags' => '',// flags are predefined values like asc|desc|random.
+						'default' => 'no',// this attribute defaults to this if no value is given
+						'required' => false,// is this attribute required?
+					),
+				),
+			),
+		);
+	
+		return $info;
+	}
+	
+	
+	
+	/**
+	 * Words
+	 *
+	 * Usage:
+	 * {{ limit:words words="25" ending="<a href='/read-more'>Read more</a>"}}
+	 *
+	 * @return string
+	 */
+
+	function words($content = '')
+	{
+		$limit_count 	= $this->attribute('words', '');
+		$ending 		= $this->attribute('ending', '...');
+		$strip_html		= $this->attribute('strip', 'no');
+		
+		
+		$content 		= $this->content();
+		
+		if($strip_html == 'yes')
+		{
+			$content	= strip_tags($content);
+		}
+		
+		$new_content 	= word_limiter($content, $limit_count, $ending);
+		
+		
+ 		return $new_content;
+	}
+}
+
+/* End of file example.php */
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Limit Plugin
+
+This is a simple plugin that allows you to limit text or strip the HTML tags.
+
+## Installation
+
+Copy the limit.php file to your addons/shared\_addons/plugins or addons/site\_ref/plugins folder.
+
+## Usage
+
+
+You can either parse content in a tag pair or as a parameter called "string". The parameter will be used in case both are present.
+
+Parsing tag pair content:
+
+	{{ limit:words words="25" ending=" <a href='/read-more'>Read more</a>" }}
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    {{ /limit:words }}
+
+Output:
+
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, Read more
+	
+	
+## Parameters
+
+Words		The number of words you want to show
+Ending		Custom ending append to the end of the text. Default = "..."
+Strip		Set "yes" to strip all HTML tags.
